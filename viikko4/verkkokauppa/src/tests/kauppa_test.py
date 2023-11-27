e1da96f024b35d5d883cd699f91acc0f4ce6dc4c
Pseudo Name <122291857+ravioli-maul@users.noreply.github.com>
2023-11-27 18:31:13 +0200

    T4 Add test for Kauppa.poista_korista

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -142,3 +142,15 @@ class TestKauppa(unittest.TestCase):
         # Tarkastetaan että viitegeneraattoria on kutsuttu kolmesti testin aikana
         self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 3)
 
+    def test_poista_korista_poistaa_tuotteen_korista(self):
+        # Lisätään ja poistetaan korista, maksun tulee olla nolla.
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.poista_korista(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with(
+        "pekka", ANY, "12345", ANY, 0
+    )
+
+
